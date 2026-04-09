9a0d2f83c85a89f0ba1393086d72279070b5643d
Wiktor Szelag <wiktor.szelag12@gmail.com>
2026-04-09 23:05:32 +0200

    Add index page with prices, registration, and update admin credentials

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ ALTER TABLE IF EXISTS uzytkownik DROP CONSTRAINT IF EXISTS uzytkownik_rola_check
 ALTER TABLE IF EXISTS uzytkownik DROP CONSTRAINT IF EXISTS uzytkownik_rola_check1;
 
 INSERT INTO uzytkownik (username, password, rola) VALUES
-('admin', 'admin', 'ADMIN'),
+('admin', 'admin123', 'ADMIN'),
 ('pracownik', 'pracownik', 'PRACOWNIK'),
 ('klient', 'klient', 'KLIENT')
 ON CONFLICT (username) DO NOTHING;
